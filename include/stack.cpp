c3b704fc46983f1dfcd9188e84572cd525108115
anastasiya0304 <89684777770@mail.ru>
2016-09-17 20:23:27 +0300

    Create stack.cpp

---
+++ include/stack.cpp
@@ -0,0 +1,26 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+using namespace std;
+
+template <typename T>
+class stack
+{
+public:
+	stack();
+	size_t count() const;
+	void push(T const &);
+	T pop();
+	~stack();
+	stack(const stack &b);
+	stack & operator=(const stack &b);
+	bool operator==(stack const & _s);
+private:
+	T * array_;
+	size_t array_size_;
+	size_t count_;
+};
+#include "stack.cpp"
+#endif
