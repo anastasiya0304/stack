a3775f985dffa2b4ac58b4e9f1b445d832b0ca6a
anastasiya0304 <89684777770@mail.ru>
2016-09-17 20:41:53 +0300

    Update and rename stack.hpp to stack.cpp

--- include/stack.cpp
+++ include/stack.cpp
@@ -1,22 +1,6 @@
-#include "stack.cpp"
+#include "stack.hpp"
 using namespace std;
-template <typename T>
-class stack
-{
-public:
-	stack();
-	size_t count() const;
-	void push(T const &);
-	stack & operator=(const stack &b);
-        bool operator==(stack const & _s);
-	T top();
-	T pop();
-        ~stack();
-private:
-	T * array_;
-	size_t array_size_;
-	size_t count_;
-};
+
 template <typename T>
 stack<T>::stack() : array_(nullptr), array_size_(0)
 {}
