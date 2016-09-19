dfcc87d558d4305bdd7fd70ba7e4aef31dc6451c
anastasiya0304 <89684777770@mail.ru>
2016-09-19 11:30:01 +0300

    Update stack.cpp

--- include/stack.cpp
+++ include/stack.cpp
@@ -15,34 +15,24 @@ stack<T>::~stack()
 delete[] array_;
 }
 
-template <typename T>
+template <typename T> 
 void stack <T> :: push(const T& b)
 {
-	
-		if (!count_)
-		{
-			array_ = new T[1];
-			array_[count_] = b;
-			array_size_ = 1;
-			count_++;
-		}
-		else if (count_ < array_size_)
-		{
-			T * s_ = new T[array_size_];
-			for (i = 0; i < array_size_; i++)
-				s_[i] = array_[i];
-			delete[] array_;
-			array_size_ = array_size_ * 2;
-			array_ = new T[array_size_];
-			for (i = 0; i < count_; i++)
-				array_[i] = s_[i];
-			delete[] s_;
-			array_[count_] = b;
-			count_++;
-		}
-
-
+	if(count_==array_size_)
+	{array_size_*=2;
+	T*l=copy_new(array_,count_,array_size_);
+	delete[] array_;
+	array_=l;
+	l=nullptr;}
+	array_[count_]=b;
+	count_++;
 }
+template<typename T>
+T*stack<T>::copy_new(const T*arr,size_t count,size_t array_size)
+{T*l=new T[array_size];
+std::copy(arr,arr+count,l);
+return;}
+
 template<typename T>
 stack<T>::top() T& {
 	if (count_ == 0) {
