00af89f451acbe783490e992e7774c7b197e9d89
anastasiya0304 <89684777770@mail.ru>
2016-11-14 18:09:23 +0400

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,45 +1,47 @@
-#include <stack.hpp>
+#include "stack.hpp"
 #include <catch.hpp>
 #include <iostream>
-using namespace std;
+#include <fstream>
 
-SCENARIO("count", "[count]"){
-  stack<int> s;
-  s.push(1);
-  REQUIRE(s.count()==1);
+SCENARIO("Stack init", "[init]") {
+	stack<int> Stack;
+	REQUIRE(Stack.count() == 0);
+}
+SCENARIO("Stack push and top", "[push and top]") {
+	stack<int> Stack;
+	Stack.push(3);
+	REQUIRE(Stack.top() == 3);
+}
+SCENARIO("Stack pop", "[pop]") {
+	stack<int> Stack;
+	for (int i = 0; i < 10; i++) {
+		Stack.push(i);
+	}
+	Stack.pop();
+	REQUIRE(Stack.count() == 9);
 }
 
-SCENARIO("push", "[push]"){
-  stack<int> s;
-  s.push(1);
-  REQUIRE(s.count()==1);
-  REQUIRE(s.top()==1);
+SCENARIO("Stack count", "[count]") {
+	stack<int> Stack;
+	for (int i = 0; i < 10; i++) {
+		Stack.push(i);
+	}
+	REQUIRE(Stack.count() == 10);
 }
 
-SCENARIO("pop", "[pop]"){
-  stack<int> s;
-  s.push(1);
-  s.pop();
-  REQUIRE(s.count()==0);
+SCENARIO("operator =", "[operator =]") {
+	stack<int> Stack;
+	Stack.push(221);
+	stack<int> Stack_ = Stack;
+	REQUIRE(Stack_.top() == 221);
 }
-
-SCENARIO("cop", "[cop]"){
-   stack<int> s;
-   s.push(1);
-   stack<int> s2=s;
-   REQUIRE(s.count()==1);
-   REQUIRE(s.top()==1);
- }
-
-SCENARIO("top", "[top]"){
-  stack<int> s;
-  s.push(1);
-  REQUIRE(s.top()==1);
+SCENARIO("copy constructor", "[copy constructor]") {
+	stack<int> Stack;
+	Stack.push(221);
+	stack<int> Stack_(Stack);
+	REQUIRE(Stack_.top() == 221);
 }
-
-SCENARIO("empty", "[empty]"){
-  stack<int> s1, s2;
-  s1.push(1);
-  REQUIRE(!s1.empty());
-  REQUIRE(s2.empty());
+SCENARIO("empty", "[empty]") {
+	stack<int> Stack;
+	REQUIRE(Stack.empty());
 }
